4f86a4896b2e69635f65632708ce83f6bac148c2
cct1225 <grace951225@gmail.com>
2023-09-17 18:23:32 +0800

    apcs

---
+++ 2023_June/202306路徑偵測.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,right=0,left=0,back=0,x[101],y[101];
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>x[i]>>y[i];
+    }
+    x[0]=0;
+    y[0]=0;
+    
+    cout<<left<<" "<<right<<" "<<back;
+
+
+
+
+
+
+
+
+}
